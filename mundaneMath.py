f60b6c2f8053a56b4d52b138102302a9d2c5a8c6
ssalen1 <47227356+ssalen1@users.noreply.github.com>
2019-02-17 20:44:04 -0500

    Update mundaneMath.py

--- mundaneMath.py
+++ mundaneMath.py
@@ -4,8 +4,8 @@ for weekly challenge 3
 """
 import numpy
 
-evennumbers = numpy.arange(2, 102, 2)
+even_numbers = numpy.arange(2, 102, 2)
 
-answer = sum(evennumbers)
+sum_even_numbers = sum(even_numbers)
 
-print("The sum of even number between 1 and 100 is {0}".format(str(answer)))
+print("The sum of even number between 1 and 100 is {0}".format(str(sum_even_numbers)))
